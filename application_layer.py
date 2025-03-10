17d760ba78532905a3ec436bdbce2678a1c97a18
cess-m <pbparages@up.edu.ph>
2025-03-10 16:32:07 +0800

    Update application_layer.py

--- application_layer.py
+++ application_layer.py
@@ -1,9 +1,15 @@
 import json
 
-def send(message):
-    request = {"method": "GET", "data": message} 
-    return json.dumps(request)
+class ApplicationLayer:
+    def send(self, message):
+        """ Simulate sending an HTTP-like request """
+        request = {"method": "GET", "data": message} 
+        print(f"Application Layer (send): Sending request with data: {message}\n")
+        return json.dumps(request)
 
-def receive(request):
-    request = json.loads(request)
-    return f"Response: Received {request['data']}"
+    def receive(self, request):
+        """ Simulate receiving an HTTP-like response """
+        request = json.loads(request)
+        response = f"Response: Received {request['data']}"
+        print(f"Application Layer (receive): {response}\n")
+        return response
