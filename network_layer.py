f54eb020a83a00b6a0cfc9fcafb953dbda70293c
cess-m <pbparages@up.edu.ph>
2025-03-10 16:32:53 +0800

    Update network_layer.py

--- network_layer.py
+++ network_layer.py
@@ -1,9 +1,23 @@
 import pickle
+import socket
 
-def send(data, ip_address="192.168.1.1"):
-    packet = {"ip": ip_address, "payload": data}
-    return pickle.dumps(packet)  # Use Pickle
+class NetworkLayer:
+    def __init__(self):
+        """ Automatically determine the local machine's IP address """
+        self.ip_address = self.get_local_ip()
 
-def receive(packet):
-    packet = pickle.loads(packet)  # Deserialize packet
-    return packet["payload"]
+    def get_local_ip(self):
+        """ Get the local IP address of the machine """
+        return socket.gethostbyname(socket.gethostname())
+
+    def send(self, data):
+        """ Simulate IP packet transmission """
+        packet = {"ip": self.ip_address, "payload": data}
+        print(f"Network Layer (send): Sending data from IP {self.ip_address}\n")  
+        return pickle.dumps(packet)  # Serialize data
+
+    def receive(self, packet):
+        """ Simulate IP packet reception """
+        packet = pickle.loads(packet)  # Deserialize packet
+        print(f"Network Layer (receive): Received data at IP {packet['ip']}\n")  
+        return packet["payload"]
